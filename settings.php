f3a489ecfdc494a94bd2db2c3bd5bb8a2c44fc05
ASiRoS <ASiRoS@users.noreply.github.com>
2016-10-24 23:39:18 +0600

    Update settings.php

--- settings.php
+++ settings.php
@@ -8,7 +8,7 @@ define('REDIRECT_URI', 'https://oauth.vk.com/blank.html');
 define('VK_API_VERSION', '3.0');
 
 /** Access token **/
-define('ACCESS_TOKEN', 'f9acada93b7bf0193d597af0bd26740da7610436d09a0a14e190bd219ee4567b6ffe10e172b4b22e661a4');
+define('ACCESS_TOKEN', '');
 
 /** Images dir **/
 define('IMAGES_DIR', dirname(__FILE__).'/images');
